708e141f821cf72634d53cd6855394a45a0318aa
Johnny Conde <jconde@hqc.com>
2025-08-20 12:31:21 -0700

    Fix SSR error on auth page with window check

--- app/auth/page.js
+++ app/auth/page.js
@@ -1,36 +1,40 @@
 "use client";
 
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
-import { Auth } from '@supabase/auth-ui-react';
-import { ThemeSupa} from '@supabase/auth-ui-shared';
-import Link from 'next/link';
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import Link from "next/link";
 
 export default function AuthPage() {
-     const supabase = createClientComponentClient();
-    
-    return (
-        <>
-            <div id="AuthPage" className="w-full min-h-screen bg-white">
-                <div className="w-full flex items-center justify-center p-5 border-b-gray-300">
-                    <Link href="/" className="min-2-[170px]">
-                        <img width="170" src="/images/solport.svg"/>
-                    </Link>
-                </div>
+  const supabase = createClientComponentClient();
 
-                <div className="w-full flex items-center justify-center p-5 border-b-gray-300">
-                    Login / Register
-                </div>
+  return (
+    <>
+      <div id="AuthPage" className="w-full min-h-screen bg-white">
+        <div className="w-full flex items-center justify-center p-5 border-b-gray-300">
+          <Link href="/" className="min-2-[170px]">
+            <img width="170" src="/images/solport.svg" />
+          </Link>
+        </div>
 
-                <div className="max-w-[400px] mx-auto px-2">
-                    {<Auth
-                        onlyThirdPartyProviders
-                        redirectTo={`${window.location.origin}/auth/callback`}
-                        supabaseClient={supabase}
-                        providers={['google']}
-                        appearance={{theme: ThemeSupa}}
-                    /> }
-                </div>
-            </div>
-        </>
-    )
+        <div className="w-full flex items-center justify-center p-5 border-b-gray-300">
+          Login / Register
+        </div>
+
+        <div className="max-w-[400px] mx-auto px-2">
+          <Auth
+            onlyThirdPartyProviders
+            redirectTo={
+              typeof window !== "undefined"
+                ? `${window.location.origin}/auth/callback`
+                : undefined
+            }
+            supabaseClient={supabase}
+            providers={["google"]}
+            appearance={{ theme: ThemeSupa }}
+          />
+        </div>
+      </div>
+    </>
+  );
 }
