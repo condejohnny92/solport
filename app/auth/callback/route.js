e4950a38f2fda42c23caa5564a68e9e5d374780d
Johnny Conde <jconde@hqc.com>
2025-08-23 11:41:41 -0700

    Stable version before Vercel changes

--- app/auth/callback/route.js
+++ app/auth/callback/route.js
@@ -3,13 +3,14 @@ import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 
 export async function GET(request) {
-    const requestUrl = new URL(request.url)
-    const code = requestUrl.searchParams.get('code')
+  const requestUrl = new URL(request.url)
+  const code = requestUrl.searchParams.get('code')
 
-    if (code) {
-        const supabase = createRouteHandlerClient({ cookies })
-        await supabase.auth.exchangeCodeForSession(code)
-    }
+  if (code) {
+    const supabase = createRouteHandlerClient({ cookies })
+    await supabase.auth.exchangeCodeForSession(code)
+  }
 
-    return NextResponse.redirect(requestUrl.origin)
+  // URL to redirect to after sign in process completes
+  return NextResponse.redirect(requestUrl.origin)
 }
